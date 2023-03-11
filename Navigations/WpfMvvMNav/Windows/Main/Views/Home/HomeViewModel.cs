8abed21de28f1a1d3e141a178226008a658ffb4d
Nikola <keha39@yandex.ru>
2023-03-11 15:17:48 +0300

    create WpfMvvMNav

---
+++ Navigations/WpfMvvMNav/Windows/Main/Views/Home/HomeViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using WpfMvvMNav.Shared.Navigations;
+using WpfMvvMNav.Windows.Main.Views.Work;
+
+namespace WpfMvvMNav.Windows.Main.Views.Home
+{
+    internal partial class HomeViewModel : ObservableRecipient
+    {
+        public string TextLabel { get; set; } = "HomeView";
+        public string TextButton { get; set; } = "GoToWorkView";
+
+        [RelayCommand]
+        private void GoToWorkView()
+        {
+            Messenger.Send(new NavigationMessage(nameof(WorkViewModel)));
+            //WeakReferenceMessenger.Default.Send(new NavigationMessage(nameof(WorkViewModel)));
+        }
+    }
+}
---
+++ Navigations/WpfMvvMNav/Windows/Main/Views/Work/WorkViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using WpfMvvMNav.Shared.Navigations;
+using WpfMvvMNav.Windows.Main.Views.Home;
+
+namespace WpfMvvMNav.Windows.Main.Views.Work
+{
+    internal partial class WorkViewModel : ObservableRecipient
+    {
+        public string TextLabel { get; set; } = "WorkView";
+        public string TextButton { get; set; } = "GoToHomeView";
+
+        [RelayCommand]
+        private void GoToHomeView()
+        {
+            Messenger.Send(new NavigationMessage(nameof(HomeViewModel)));
+            //WeakReferenceMessenger.Default.Send(new NavigationMessage(nameof(HomeViewModel)));
+        }
+    }
+}
